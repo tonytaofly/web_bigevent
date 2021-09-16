060c0d2d88086a864b3cd989abd2156cd8a2b188
tonytao <1518772283@qq.com>
2021-09-16 22:46:37 +0800

    第一天的工作量（3）

--- assets/js/login.js
+++ assets/js/login.js
@@ -15,4 +15,12 @@ $(function() {
 
     // 从layui中获取form对像
     var form = layui.form
+    // 自定义规则
+    form.verify({
+        //自己定义了一个叫做pwd的校验规则
+        pwd: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+          ] 
+    })
 })
