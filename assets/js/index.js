856a31b1d7a26d94726ea849fa2317e41df7a8ba
tonytao <1518772283@qq.com>
2021-09-25 17:59:02 +0800

    完成了主页的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,58 @@
-$(function(){
+$(function () {
     // 调用函数获取信息
     getUserInfo()
+
+    layer = layui.layer
+    $('#btnLogout').on('click', function () {
+        //  提示用户是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+
+            // 关闭 confirm 询问框
+            layer.close(index);
+        });
+    })
 })
 
 // 获取用户的基本信息
-function getUserInfo(){
+function getUserInfo() {
     $.ajax({
-        method:'GET',
-        url:'/my/userinfo',
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-          },
-          success:function(res){
-              if(res.status !== 0){
-                  return layui.layer.msg('获取用户信息失败')
-              }
-              
-          }
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        //   },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            //  调用函数渲染头像
+            renderAvatar(res.data)
+        }
     })
 }
+
+
+//渲染用户头像
+function renderAvatar(user) {
+    // 获取用户名
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 按需渲染头像
+    if (user.user_pic !== null) {
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        //渲染文本头像 
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
+
