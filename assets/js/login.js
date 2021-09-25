dbb8c6226fc415458cd6f7af06bf1c431a481e8c
tonytao <1518772283@qq.com>
2021-09-25 10:05:50 +0800

    第二天（3）

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,6 +12,7 @@ $(function () {
     })
 
     var form = layui.form
+    var layer = layui.layer
     form.verify({
         // 自定义了一个叫做 pwd 校验规则
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
@@ -39,13 +40,35 @@ $(function () {
             username: $('#form_reg [name=username]').val(),
             password: $('#form_reg [name=password]').val()
         }
-        $.post('http://api-breakingnews-web.itheima.net', data, function (res) {
+        $.post('/api/reguser', data, function (res) {
             if (res.status !== 1) {
                 return layer.msg(res.message)
             }
             layer.msg('注册成功，请登录！')
             // 模拟人的点击行为
             $('#link_login').click()
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function (e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 1) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
         })
     })
 })
